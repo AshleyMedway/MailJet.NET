ea23cc16f9bd483f71888f1ca44416ee064e751e
Ashley Medway <ashleymedway@me.com>
2015-06-03 08:38:16 +0100

    Clean formatting

--- MailJet.Client.Tests/SendMail.cs
+++ MailJet.Client.Tests/SendMail.cs
@@ -1,44 +1,43 @@
-﻿using System;
+﻿using NUnit.Framework;
 using System.Net.Mail;
-using NUnit.Framework;
 
 namespace MailJet.Client.Tests
 {
-	[TestFixture]
-	public class SendMail
-	{
-		private MailJetClient _client;
+    [TestFixture]
+    public class SendMail
+    {
+        private MailJetClient _client;
 
-		[SetUp]
-		public void Setup()
-		{
-			_client = new MailJetClient("xxx", "xxx");
-		}
+        [SetUp]
+        public void Setup()
+        {
+            _client = new MailJetClient("xxx", "xxx");
+        }
 
-		[Test]
-		public void MailMessage_Text_NoAttachements()
-		{
-			var message = new MailMessage();
-			message.From = new MailAddress("myemail@mydomain.com");
-			message.To.Add(new MailAddress("anotheremail@mydomain.com"));
-			message.Subject = "test";
-			message.Body = "test";
-			var result = _client.SendMessage(message);
-			Assert.IsTrue(result);
-		}
+        [Test]
+        public void MailMessage_Text_NoAttachements()
+        {
+            var message = new MailMessage();
+            message.From = new MailAddress("myemail@mydomain.com");
+            message.To.Add(new MailAddress("anotheremail@mydomain.com"));
+            message.Subject = "test";
+            message.Body = "test";
+            var result = _client.SendMessage(message);
+            Assert.IsTrue(result);
+        }
 
-		[Test]
-		public void MailMessage_Html_NoAttachements()
-		{
-			var message = new MailMessage();
-			message.From = new MailAddress("myemail@mydomain.com");
-			message.To.Add(new MailAddress("anotheremail@mydomain.com"));
-			message.Subject = "test";
-			message.Body = "<b>TEST</b>";
-			message.IsBodyHtml = true;
-			var result = _client.SendMessage(message);
-			Assert.IsTrue(result);
-		}
-	}
+        [Test]
+        public void MailMessage_Html_NoAttachements()
+        {
+            var message = new MailMessage();
+            message.From = new MailAddress("myemail@mydomain.com");
+            message.To.Add(new MailAddress("anotheremail@mydomain.com"));
+            message.Subject = "test";
+            message.Body = "<b>TEST</b>";
+            message.IsBodyHtml = true;
+            var result = _client.SendMessage(message);
+            Assert.IsTrue(result);
+        }
+    }
 }
 
--- MailJetClient/MailJetClient.cs
+++ MailJetClient/MailJetClient.cs
@@ -1,81 +1,81 @@
 ﻿using System;
-using System.Net.Mail;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Net.Mail;
 using System.Text;
-using System.Collections.Generic;
 using System.Threading.Tasks;
-using System.Net;
-using System.Linq;
 
 namespace MailJet.Client
 {
-	public class MailJetClient
-	{
-		private readonly string _publicKey;
-		private readonly string _privateKey;
+    public class MailJetClient
+    {
+        private readonly string _publicKey;
+        private readonly string _privateKey;
 
-		public MailJetClient (string PublicKey, string PrivateKey)
-		{
-			_publicKey = PublicKey;
-			_privateKey = PrivateKey;
-		}
+        public MailJetClient(string PublicKey, string PrivateKey)
+        {
+            _publicKey = PublicKey;
+            _privateKey = PrivateKey;
+        }
 
-		public bool SendMessage(MailMessage Message)
-		{
-			using (var client = WebClient)
-			{
-				var data = new List<KeyValuePair<string, string>>();
-				data.Add(new KeyValuePair<string, string>("from", Message.From.Address));
-				data.Add(new KeyValuePair<string, string>("subject", Message.Subject));
+        public bool SendMessage(MailMessage Message)
+        {
+            using (var client = WebClient)
+            {
+                var data = new List<KeyValuePair<string, string>>();
+                data.Add(new KeyValuePair<string, string>("from", Message.From.Address));
+                data.Add(new KeyValuePair<string, string>("subject", Message.Subject));
 
-				var recipientsCount = Message.To.Count;
-				recipientsCount += Message.CC.Count;
-				recipientsCount += Message.Bcc.Count;
+                var recipientsCount = Message.To.Count;
+                recipientsCount += Message.CC.Count;
+                recipientsCount += Message.Bcc.Count;
 
-				if (recipientsCount == 0)
-					throw new InvalidOperationException("Must have at least one recipient. http://dev.mailjet.com/guides/send-api-guide/");
+                if (recipientsCount == 0)
+                    throw new InvalidOperationException("Must have at least one recipient. http://dev.mailjet.com/guides/send-api-guide/");
 
-				if (recipientsCount > 50)
-					throw new InvalidOperationException("Max Recipients is 50. http://dev.mailjet.com/guides/send-api-guide/");
+                if (recipientsCount > 50)
+                    throw new InvalidOperationException("Max Recipients is 50. http://dev.mailjet.com/guides/send-api-guide/");
 
-				foreach (var address in Message.To)
-					data.Add(new KeyValuePair<string, string>("to", address.Address));
+                foreach (var address in Message.To)
+                    data.Add(new KeyValuePair<string, string>("to", address.Address));
 
-				foreach (var address in Message.CC)
-					data.Add(new KeyValuePair<string, string>("cc", address.Address));
+                foreach (var address in Message.CC)
+                    data.Add(new KeyValuePair<string, string>("cc", address.Address));
 
-				foreach (var address in Message.Bcc)
-					data.Add(new KeyValuePair<string, string>("bcc", address.Address));
+                foreach (var address in Message.Bcc)
+                    data.Add(new KeyValuePair<string, string>("bcc", address.Address));
 
-				if (Message.IsBodyHtml)
-					data.Add(new KeyValuePair<string, string>("html", Message.Body));
-				else
-					data.Add(new KeyValuePair<string, string>("text", Message.Body));
+                if (Message.IsBodyHtml)
+                    data.Add(new KeyValuePair<string, string>("html", Message.Body));
+                else
+                    data.Add(new KeyValuePair<string, string>("text", Message.Body));
 
-				if (Message.Attachments.Any())
-					throw new NotImplementedException("Attachments not yet supported.");
+                if (Message.Attachments.Any())
+                    throw new NotImplementedException("Attachments not yet supported.");
 
-				if (!String.IsNullOrWhiteSpace (Message.Sender.Address))
-					throw new NotImplementedException("Sender Address not yet supported.");
+                if (!String.IsNullOrWhiteSpace(Message.Sender.Address))
+                    throw new NotImplementedException("Sender Address not yet supported.");
 
-				var content = new FormUrlEncodedContent(data);
-				var task = client.PostAsync("https://api.mailjet.com/v3/send/message", content);
-				var result = Task.Run(() => task).Result;
-				return result.StatusCode == HttpStatusCode.OK;
-			}
-		}
+                var content = new FormUrlEncodedContent(data);
+                var task = client.PostAsync("https://api.mailjet.com/v3/send/message", content);
+                var result = Task.Run(() => task).Result;
+                return result.StatusCode == HttpStatusCode.OK;
+            }
+        }
 
-		private HttpClient WebClient
-		{
-			get
-			{
-				var client = new HttpClient();
-				client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("basic",
-					Convert.ToBase64String(Encoding.ASCII.GetBytes(String.Format("{0}:{1}", _publicKey, _privateKey))));
-				return client;
-			}
-		}
-	}
+        private HttpClient WebClient
+        {
+            get
+            {
+                var client = new HttpClient();
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("basic",
+                    Convert.ToBase64String(Encoding.ASCII.GetBytes(String.Format("{0}:{1}", _publicKey, _privateKey))));
+                return client;
+            }
+        }
+    }
 }
 
