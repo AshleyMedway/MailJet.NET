9a7e5bda74a75b5a7b5f49a91f3ef5c63f239681
Ashley Medway <ashleymedway@me.com>
2015-12-13 21:06:42 +0000

    Made updated a bunch of IDs to long(Int64) over int(Int32).

--- MailJetClient/Response/Data/MessageData.cs
+++ MailJetClient/Response/Data/MessageData.cs
@@ -8,18 +8,19 @@ namespace MailJet.Client.Response.Data
         public DateTime ArrivedAt { get; set; }
         public int AttachmentCount { get; set; }
         public int AttemptCount { get; set; }
-        public int CampaignID { get; set; }
-        public int ContactID { get; set; }
+        public long CampaignID { get; set; }
+        public long ContactID { get; set; }
         public long Delay { get; set; }
         public int DestinationID { get; set; }
         public int FilterTime { get; set; }
-        public int FromID { get; set; }
+        public long FromID { get; set; }
         public bool IsClickTracked { get; set; }
         public bool IsHtmlPartIncluded { get; set; }
         public bool IsOpenTracked { get; set; }
         public bool IsUnsubTracked { get; set; }
         public long MessageSize { get; set; }
-        public int SpamassassinScore { get; set; }
+        public decimal SpamassassinScore { get; set; }
+        public long StateID { get; set; }
         public bool StatePermanent { get; set; }
         public MessageStatus Status { get; set; }
     }
